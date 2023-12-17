8adfbec8d565b3b1f7260e640ca6bf7ff197db0e
Andrés González Flores <gandresto@outlook.com>
2023-12-16 21:08:59 -0600

    feat: add /v1/healthz readiness handler

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -21,6 +22,21 @@ func main() {
 	}
 
 	router := chi.NewRouter()
+
+	// Basic CORS
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
 
 	server := &http.Server{
 		Handler: router,
