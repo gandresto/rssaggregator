9104475cf74ab35094987b95dda6dd63c5c3a20f
Andrés González Flores <gandresto@outlook.com>
2023-12-21 14:10:57 -0600

    feat: map database user to user with json tags

--- handler_user.go
+++ handler_user.go
@@ -35,5 +35,5 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	respondWithJSON(w, 201, user)
+	respondWithJSON(w, 201, databaseUserToUser(user))
 }
